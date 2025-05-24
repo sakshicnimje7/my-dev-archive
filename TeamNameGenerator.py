50db8b128588134fce5ee83657e4c4a6286ffdc6
Sakshi Nimje <sakshinimje7@gmail.com>
2025-05-24 21:29:48 +0530

    Project-01 (mini)
    A versatile Team Name Generator designed to assist in creating unique and engaging names for various groups, including hackathon teams, project squads, and sports clubs.

---
+++ TeamNameGenerator.py
@@ -0,0 +1,4 @@
+print("Hi! I am a Team Name Generator.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is name of your pet animal?\n")
+print("Your team name could be: " + city + " " + pet)
